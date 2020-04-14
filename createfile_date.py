6f1724ca4edca82907953aa67a8c335a71b05b63
yukito goto <ykt3693@outlook.jp>
2020-04-14 15:04:34 +0900

    update comment & add function "write_hojo_id"

--- createfile_date.py
+++ createfile_date.py
@@ -20,7 +20,14 @@ def get_hojo(PATH):
     file.close()
     return hojo_list
 
-#指定されたメタ情報でフォルダーを作成 作成したフォルダーidを取得:Ture
+#圃場フォルダーのIDをhojo_id.txtに書き込む hojo_list.txtと同じ順番に書き込むこと
+def write_hojo_id(PATH,ID,MODE):
+    file = open(PATH,MODE)
+    file.write(ID + "\n")
+    file.close()
+    return ID + "\n"
+
+#指定されたメタ情報でフォルダーを作成 作成したフォルダーIDを取得:BOOL = Ture
 def create_folder(META,BOOL):
     f = drive.CreateFile(META)
     if(f.Upload() == None):
@@ -35,15 +42,20 @@ def create_folder(META,BOOL):
         #upload失敗
         return "upload error"
 
+"""
+各種定数 "inital_value":プログラム中で取得
+"""
 #関連のpath
 FOLDER_LOG_PATH = "folder/folder_log.txt"
 HOJO_LIST_PATH = "folder/hojo_list.txt"
+HOJO_ID_PATH = "folder/hojo_id.txt"
 #日付フォルダー名
 TODAY_FOLDER_NAME = get_today()
 HOJO_FOLDER_NAME = "initial_value"
 #各フォルダーの親フォルダーID
 PARENTS_TODAY_FOLDER_ID = "1UeneN_9RGHeLy2zKtxlgssyIDnXkG1dG"
 PARENTS_HOJO_FOLDER_ID = "initial_value"
+HOJO_FOLDER_ID = "inital_value"
 #作成するフォルダーのメタ情報
 TODAY_FOLDER_META = {"title": TODAY_FOLDER_NAME, "mimeType": "application/vnd.google-apps.folder", "parents": [{"id": PARENTS_TODAY_FOLDER_ID}]}
 HOJO_FOLDER_META = {"title": "initial_value", "mimeType": "application/vnd.google-apps.folder", "parents": [{"id": "initial_value"}]}
@@ -57,10 +69,15 @@ if(len(file_list) == 0):
     PARENTS_HOJO_FOLDER_ID = create_folder(TODAY_FOLDER_META,True)
     #取得したフォルダーIDを親フォルダーとしてメタ情報に反映
     HOJO_FOLDER_META["parents"][0]["id"] = PARENTS_HOJO_FOLDER_ID
-    for name in get_hojo(HOJO_LIST_PATH):
+    #圃場名をlistで取得 index:カウンタ（listの添え字）
+    for index,name in enumerate(get_hojo(HOJO_LIST_PATH)):
         HOJO_FOLDER_NAME = name
         #取得した圃場名をメタ情報に反映
         HOJO_FOLDER_META["title"] = HOJO_FOLDER_NAME
-        create_folder(HOJO_FOLDER_META,False)
+        #HOJO_FOLDER_NAMEを作成 作成した圃場フォルダーのIDを取得
+        HOJO_FOLDER_ID = create_folder(HOJO_FOLDER_META,True)
+        #最初は上書きモード:"w" それ以降は追記モード:"a"
+        mode = "w" if (index == 0) else "a"
+        write_hojo_id(HOJO_ID_PATH,HOJO_FOLDER_ID,mode)
 else:
     print("already exist")
