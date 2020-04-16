587cec9fcbc46a7b168988f9b14d31f65f401c1f
yukito goto <ykt3693@outlook.jp>
2020-04-16 13:33:10 +0900

    add comment

--- imageupload.py
+++ imageupload.py
@@ -2,10 +2,14 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
+#コマンドラインから認証
 gauth = GoogleAuth()
 gauth.CommandLineAuth()
 drive = GoogleDrive(gauth)
 
+#Googleドライブオブジェクト生成
 f = drive.CreateFile({'title': 'test.jpg', 'mimeType': 'image/jpeg'})
+#ローカルファイルをセット
 f.SetContentFile('picture/test.jpg')
+#アップロード
 f.Upload()
