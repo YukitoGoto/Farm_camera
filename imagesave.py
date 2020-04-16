8230dbee745fbc9ecbee37b54fac18c8c4e42c30
yukito goto <ykt3693@outlook.jp>
2020-04-16 13:31:09 +0900

    fix indent

--- imagesave.py
+++ imagesave.py
@@ -5,7 +5,7 @@ camera = cv2.VideoCapture(0)                # カメラCh.(ここでは0)を指�
  
 # 撮影＝ループ中にフレームを1枚ずつ取得（qキーで撮影終了）
 while True:
-    ret,frame = camera.read()              # フレームを取得
+    ret,frame = camera.read()               # フレームを取得
     cv2.imshow('camera', frame)             # フレームを画面に表示
  
     # キー操作があればwhileループを抜ける
