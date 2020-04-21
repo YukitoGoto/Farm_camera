84e9834832faf4f38afe48b1f11f9f7a9b1b5695
yukito goto <ykt3693@outlook.jp>
2020-04-21 14:19:55 +0900

    add function "get_hojo_id"

--- onetime_photo.py
+++ onetime_photo.py
@@ -31,8 +31,22 @@ def get_hojo(PATH):
     file.close()
     return hojo_my
 
+#自身の圃場フォルダidをhojo_id.txtを更新して取得
+def get_hojo_id(PATH,HEADNAME):
+    file = open(PATH,"r")
+    id_flag = False
+    for id in file:
+        print(id)
+        if(id.strip() == HEADNAME):
+            id_flag = True
+        elif(id_flag == True):
+            return id
+        else:
+            pass
+    return "get_hojo_id_error"
+
 """
-各種定数 "inital_value":プログラム中で取得
+各種定数
 """
 #カメラのID 1:内蔵 0:外部
 CAM_ID = 0
@@ -42,12 +56,13 @@ PICTURE_PATH = "picture/"
 HOJO_MY_PATH = "hojo/hojo_my.txt"
 HOJO_ID_PATH = "hojo/hojo_id.txt"
 #圃場名
-HOJO = get_hojo(HOJO_PATH)
+HOJO = get_hojo(HOJO_MY_PATH)
 #圃場フォルダーID
-FOLDER_ID = "inital_value"
+HOJO_FOLDER_ID = get_hojo_id(HOJO_ID_PATH,HOJO)
 
 #start
 up_log(PICTURE_LOG_PATH,get_log("START","log"))
+"""
 #カメラをオープン
 cam = cv2.VideoCapture(CAM_ID)
 
@@ -60,7 +75,7 @@ else:
         if(read_check):
             picture_name = get_log(HOJO,"picture") + ".png"
             cv2.imwrite(PICTURE_PATH + picture_name,frame_imfo)
-            f = drive.CreateFile({"title": picture_name, "mimeType": "image/png","parents": [{"kind": "drive#fileLink", "id": FOLDER_ID}]})
+            f = drive.CreateFile({"title": picture_name, "mimeType": "image/png","parents": [{"kind": "drive#fileLink", "id": HOJO_FOLDER_ID}]})
             f.SetContentFile(PICTURE_PATH + picture_name)
             if(f.Upload() == None):
                 up_log(PICTURE_LOG_PATH,get_log("upload done","log"))
@@ -70,6 +85,6 @@ else:
 
 #カメラをクローズ
 cam.release()
-
+"""
 #finish
 up_log(PICTURE_LOG_PATH,get_log("FINISH","log"))
